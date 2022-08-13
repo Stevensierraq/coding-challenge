62fca4c5c496a3aab27fd0e38c16d9df4f658c8b
Stevensierraq <stevensierra@Stevens-MacBook-Pro.local>
2022-08-13 10:09:53 -0500

    feat(loggin): add sorted solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,27 @@
 "use strict";
+const { getAsyncLogs, sortLogsByDate, resolveAsyncLogs } = require("../lib/utils")
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
 module.exports = (logSources, printer) => {
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
+    
+    const logsPromisesEntries = logSources.reduce((acc, logEntry) => {
+      const logs = getAsyncLogs(logEntry)
+      return [...acc, logs]
+    }, []);
+    
+    const logsEntries = resolveAsyncLogs(logsPromisesEntries);
+
+    logsEntries.then(logs => {
+      const sortedLogs = sortLogsByDate(logs);
+      sortedLogs.map(log => printer.print(log));
+      printer.done();
+  
+  
+      resolve(console.log("Async sort complete."));
+    });
+  })
+    
+  
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,18 @@
 "use strict";
+const _ = require("lodash");
+const { getSyncLogs, sortLogsByDate } = require("../lib/utils")
 
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  const logsEntries = logSources.reduce((acc, logEntry) => {
+    const logs = getSyncLogs(logEntry)
+    return [...acc, ...logs]
+  }, []);
+
+  const sortedLogs = sortLogsByDate(logsEntries);
+  sortedLogs.map(log => printer.print(log));
+  printer.done();
+
   return console.log("Sync sort complete.");
 };
